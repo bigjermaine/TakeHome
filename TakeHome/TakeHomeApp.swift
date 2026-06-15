14b818c7dd6a8d080794521ba3328030ed8a7d0d
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2026-06-15 17:41:44 +0100

    Initial Commit

---
+++ TakeHome/TakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeApp.swift
+//  TakeHome
+//
+//  Created by jermaine daniel on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
